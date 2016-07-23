decfceca868210e752d3c920faeb735e6acbc704
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-23 19:29:24 -0300

    Add log in TemporaryFileDAO

--- src/temporary_file_dao.rb
+++ src/temporary_file_dao.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 
+require 'logger'
+
 class TemporaryFileDAO
   def initialize
     @tmp_path = 'tmp/'
     @seeds_path = @tmp_path + 'seeds/'
+    @log = Logger.new(STDOUT)
+    @log.level = Logger::INFO
   end  
 
   def save_seed_json(file_name, json)
     File.open(@seeds_path + file_name, 'w') do |file|
       file.write(json.to_json)
     end
+    
+    @log.info("File " + file_name + "saved in temporary diretory.")
   end
 end
 
