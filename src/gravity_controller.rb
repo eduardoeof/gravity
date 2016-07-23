ee26c45173723a50eefccc4555b9cd411a1fea10
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-23 14:42:57 -0300

    Create main and GravityController files

---
+++ src/gravity_controller.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class GravityController
+
+  def download_datas
+    puts 'Starting download...'
+  end    
+
+end
+
---
+++ src/main.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require_relative 'gravity_controller'
+
+controller = GravityController.new()
+controller.download_datas()
+
