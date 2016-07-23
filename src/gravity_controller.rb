6fdafe9e51edc0ca8fe691b43f9d393c9300ad20
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-23 19:06:12 -0300

    Save downloaded json using TemporaryFileDAO in GravityController

--- src/gravity_controller.rb
+++ src/gravity_controller.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 
 require_relative 'lol_seed_data_connector'
+require_relative 'temporary_file_dao'
 
 class GravityController
   def initialize
     @seed_connector = LoLSeedDataConnector.new 
+    @tmp_dao = TemporaryFileDAO.new 
   end
 
   def start_downloads
-    file = @seed_connector.fetch(file_name='matches1.json')
-    puts file
+    file_name = 'matches1.json'
+    match_json = @seed_connector.fetch(file_name)
+
+    @tmp_dao.save_seed_json(file_name=file_name, json=match_json)
   end    
 end
 
