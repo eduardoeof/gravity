e4d63b4114137db8c8ef8abfa0f7ecec44b1e8fc
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-23 19:29:07 -0300

    Add log in LoLSeedDataConnector

--- src/lol_seed_data_connector.rb
+++ src/lol_seed_data_connector.rb
@@ -1,17 +1,25 @@
 #!/user/bin/env ruby
 
-require 'net/http'
 require 'json'
+require 'logger'
+require 'net/http'
 
 class LoLSeedDataConnector
   def initialize()
     @url = 'https://s3-us-west-1.amazonaws.com/riot-api/seed_data/'
+    @log = Logger.new(STDOUT)
+    @log.level = Logger::INFO
   end
 
   def fetch(file_name)
+    @log.info("Start download " + file_name)
+    
     url = @url + file_name
     uri = URI(url)
     response = Net::HTTP.get(uri)
+    
+    @log.info("Download " + file_name + " succeed")
+   
     return JSON.parse(response)    
   end
 end
