d4ae547074b66b3654ac824114b3f39df77dafe5
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-23 19:34:28 -0300

    Add log in main file

--- src/main.rb
+++ src/main.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
+require 'logger'
+
 require_relative 'gravity_controller'
 
 if __FILE__ == $0
+  log = Logger.new(STDOUT)
+  log.level = Logger::INFO
+
+  log.info("Gravity start running!")
+
   controller = GravityController.new()
   controller.start_downloads()
+  
+  log.info("Gravity finished its job. That\'s all folks!")
 end
 
