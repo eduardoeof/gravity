716324f63a158521f9d0f68ce12e2554cef87e5a
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-24 12:27:14 -0300

    Check if seed files exist and load them if exist in GravityController - This avoid request if data files exist.

--- src/gravity_controller.rb
+++ src/gravity_controller.rb
@@ -14,15 +14,19 @@ class GravityController
       file_name = 'matches' + i.to_s + '.json'
 
       matches_json = nil
-      if !@tmp_dao.is_seed_file_exist(file_name)
+      if @tmp_dao.is_seed_file_exist(file_name)
+        matches_json = @tmp_dao.load_seed_file(file_name)
+      else
         matches_json = @seed_connector.fetch(file_name)
         @tmp_dao.save_seed_json(file_name=file_name, json=matches_json)
       end
 
+      save_seed_matches_in_db(matches_json)
     end
   end 
 
   private def save_seed_matches_in_db(matches_json)
+    puts 'File match:'
     matches_json['matches'].each do |match|
       puts match.to_s
       break
