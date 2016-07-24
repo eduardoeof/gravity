acf1696b3bb5abd819b569515f814ca92e575dd2
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-24 12:14:47 -0300

    Create method svae_seed_matches_in_db in GravityController - This method doesn't save yet. It was just a test to get a match in json object.

--- src/gravity_controller.rb
+++ src/gravity_controller.rb
@@ -13,11 +13,20 @@ class GravityController
     for i in 1..10
       file_name = 'matches' + i.to_s + '.json'
 
+      matches_json = nil
       if !@tmp_dao.is_seed_file_exist(file_name)
         matches_json = @seed_connector.fetch(file_name)
         @tmp_dao.save_seed_json(file_name=file_name, json=matches_json)
       end
+
     end
-  end    
+  end 
+
+  private def save_seed_matches_in_db(matches_json)
+    matches_json['matches'].each do |match|
+      puts match.to_s
+      break
+    end
+  end   
 end
 
