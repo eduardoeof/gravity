8814c137ce9e18e393a3c8b220a2a26eaffc8dd8
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-28 23:35:16 -0300

    Create class GLogger - Initilizer create two logger: file and stdout. - Create method info. - Create method error.

---
+++ src/glogger.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'logger'
+
+class GLogger
+  def initialize(owner_class)
+    @owner_class_name = owner_class.name
+    
+    log_file = 'gravity_log.txt'
+    logger_file = Logger.new(log_file)
+    logger_stdout = Logger.new(STDOUT)
+
+    @loggers = [logger_file, logger_stdout]
+    define_loggers_level()
+  end
+
+  def info(message)
+    @loggers.each do |logger|
+      logger.info(message)
+    end
+  end 
+
+  def error(message)
+    @loggers.each do |logger|
+      logger.error(message)
+    end
+  end
+
+  private def define_loggers_level
+    if @loggers.nil? 
+      @loggers.each do |logger|
+        logger.level = Logger::INFO
+      end
+    end
+  end
+end
+
