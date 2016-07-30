e50719df324703701e3548762c761a2a4f368e81
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-30 19:15:21 -0300

    Remove log form main

--- src/main.rb
+++ src/main.rb
@@ -5,13 +5,7 @@ require_relative 'glogger'
 require_relative 'gravity_controller'
 
 if __FILE__ == $0
-  log = GLogger.new()
-
-  log.info("Gravity start running!")
-
   controller = GravityController.new()
   controller.start_downloads()
-  
-  log.info("Gravity finished its job. That\'s all folks!")
 end
 
