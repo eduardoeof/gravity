f0de534510e0d120b8e93ae38420c9926ef4cad9
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-07-30 18:38:54 -0300

    GLogger creates log dir to save log files

--- src/glogger.rb
+++ src/glogger.rb
@@ -8,8 +8,11 @@ class GLogger
       @owner_class_name = owner_class.name
     end    
 
+    @log_path = 'log/'
+    create_dir(@log_path)
+
     log_file = 'gravity_log.txt'
-    logger_file = Logger.new(log_file)
+    logger_file = Logger.new(@log_path + log_file)
     logger_stdout = Logger.new(STDOUT)
 
     @loggers = [logger_file, logger_stdout]
@@ -34,6 +37,10 @@ class GLogger
         logger.level = Logger::INFO
       end
     end
+  end
+
+  private def create_dir(dir_path)
+    Dir.mkdir(dir_path) unless File.exists?(dir_path)
   end
 end
 
