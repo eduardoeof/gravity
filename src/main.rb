ad46dfe03eee558ef4a097d82c38b566913ffee4
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-08-04 21:28:17 -0300

    Create dao dir

--- src/main.rb
+++ src/main.rb
@@ -5,8 +5,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'controller/gravity_controller'
 require 'connector/lol_seed_data_connector'
 require 'support/glogger'
-require 'seed_match_dao'
-require 'temporary_file_dao'
+require 'dao/seed_match_dao'
+require 'dao/temporary_file_dao'
 
 if __FILE__ == $0
   controller = GravityController.new()
