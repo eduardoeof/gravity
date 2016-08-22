dc404699a731474ee1e6c275edaa898581cf0eee
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-08-22 18:43:38 -0300

    Create method load_recent_games in GravityController - Also made main call load_recent_games instead of start_downloads.

--- src/main.rb
+++ src/main.rb
@@ -3,13 +3,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'controller/gravity_controller'
+require 'connector/lol_recent_game_connector'
 require 'connector/lol_seed_data_connector'
 require 'support/glogger'
 require 'dao/seed_match_dao'
 require 'dao/temporary_file_dao'
+require 'dao/summoner_dao'
+require 'dao/recent_game_dao'
 
 if __FILE__ == $0
   controller = GravityController.new()
-  controller.start_downloads()
+  controller.load_recent_games()
 end
 
