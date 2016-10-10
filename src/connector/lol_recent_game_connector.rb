6f2a8ff15b88631c1859319461030620225a9cae
Eduardo Ferreira <eduardo.ferreira@schibsted.com.br>
2016-10-10 00:35:39 -0300

    Use rate limit validator in LoLRecentGameConnector

--- src/connector/lol_recent_game_connector.rb
+++ src/connector/lol_recent_game_connector.rb
@@ -5,20 +5,29 @@ require 'net/http'
 
 class LoLRecentGameConnector
   def initialize
+    @rate_limit_validator = LoLRateLimitValidator.new
     @log = GLogger.new(LoLRecentGameConnector)
   end
 
   def fetch_games(summoner_id)
     @log.info('Start recent games request of summonerId ' + summoner_id)
 
+    if !@rate_limit_validator.should_send_request()
+      @rate_limit_validator.wait_until_rate_limit_permits()
+    end
+
     url = create_url(summoner_id)
     uri = URI(url)
     response = Net::HTTP.get(uri)
+
+    @rate_limit_validator.register_request()
 
     @log.info('Recent games request of summonerId ' + summoner_id + ' succeed')
 
     return parse_response_to_array(response)
   end
+
+  # Private
 
   private def create_url(summoner_id)
     api_key = 'RGAPI-2BE249D7-0CB3-4685-A4E5-6BE9381FA129'
